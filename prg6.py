7d11a3dbdaf30935ff54f483de65c388734a2b05
Manwell <m@a.ru>
2024-09-24 11:20:38 +0300

    Operators

---
+++ prg6.py
@@ -0,0 +1,10 @@
+# donut = input('Сколько стоил пончик: ')
+# tea = input('Сколько стоил чай: ')
+#
+# total = float(donut) + float(tea)
+#
+# print('С вас', total, 'руб.')
+
+string = ''
+
+print(string)
---
+++ prg7.py
@@ -0,0 +1,13 @@
+# Управляющие последовательности в строках
+# Escape sequence
+# / - slash
+# \ - backslash - начало упр. послед.
+
+string = 'Привет\n\n\n\nучастникам\n\tсоревнования'
+path = 'C:\\Windows\\com.exe'
+conflict = 'Кафе "Аист"'
+plus = '3 + 5'
+
+print(string)
+print(path)
+print(conflict)
