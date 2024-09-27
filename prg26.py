cd8fb502380f4bc408306b655e3afb58ef1dfcf1
Manwell <m@a.ru>
2024-09-27 11:58:59 +0300

    SET Example

---
+++ prg26.py
@@ -0,0 +1,16 @@
+# Пример задачи с множествами
+
+forbiden = {'l', 'O', 'I'}
+letters = {'a', 'b', 'c', 'd', 'l', 'O', 'I'}
+digits = {1, 2}
+
+# удаляем некорректные символы
+remove_incorrect = letters - forbiden
+
+# объединяем с цифрами
+temp = remove_incorrect | digits
+
+temp = list(temp)
+
+for i in range(6):
+    print(temp[i], end='') # [] - обратиться по индексу
