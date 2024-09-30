b8a6e8b696e228297d8be02f7968113e7a781987
ipapMaster <wmast@inbox.ru>
2024-09-30 09:55:55 +0300

    Slice Intro

--- prg27.py
+++ prg27.py
@@ -1,7 +1,8 @@
-# index  012345
+# index - порядковый номер элемента в последовательности
+# slice - срез [start:stop:step]
 s = 'Python'
 
-print(s[-1])
+print(s[::-1])  # разворот строки срезом
 
 length = len(s)  # для индекса отнимаем 1
 print('Длина строки', s, length)
